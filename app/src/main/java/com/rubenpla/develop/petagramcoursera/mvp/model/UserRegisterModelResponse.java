9cf5ce436f5ba4d6bb1728fe8b59706f01c6d700
Ruben Pla <ruben.pla@alten.es>
2017-08-22 12:46:23 +0200

    - Model & Model response for registered user's new functionality

---
+++ app/src/main/java/com/rubenpla/develop/petagramcoursera/mvp/model/UserRegisterModelResponse.java
@@ -0,0 +1,14 @@
+package com.rubenpla.develop.petagramcoursera.mvp.model;
+
+public class UserRegisterModelResponse {
+
+    private UserRegisterModel model;
+
+    public UserRegisterModel getModel() {
+        return model;
+    }
+
+    public void setModel(UserRegisterModel model) {
+        this.model = model;
+    }
+}
