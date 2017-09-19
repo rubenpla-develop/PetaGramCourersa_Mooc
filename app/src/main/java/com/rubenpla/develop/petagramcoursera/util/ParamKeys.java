e11164678c16d0abbdb1dbfd03ee9cc5b515af04
Ruben Pla <rubenpla.develop@gmail.com>
2017-09-19 17:19:05 +0200

    - Updated ParamKeys & PreferencesKeys

--- app/src/main/java/com/rubenpla/develop/petagramcoursera/util/ParamKeys.java
+++ app/src/main/java/com/rubenpla/develop/petagramcoursera/util/ParamKeys.java
@@ -5,4 +5,6 @@ public class ParamKeys {
     public static final String KEY_EXTRA_LIKES = "LIKES";
     public static final String KEY_EXTRA_USER_ID = "USER_ID";
     public static final String KEY_EXTRA_MEDIA_ID = "MEDIA_ID";
+    public static final String KEY_FIREBASE_PARAM = "KEY_FIREBASE_PARAM";
+    public static final String KEY_EXTRA_PROFILE_INFO = "KEY_EXTRA_PROFILE_INFO";
 }
--- app/src/main/java/com/rubenpla/develop/petagramcoursera/util/preferences/PreferencesKeys.java
+++ app/src/main/java/com/rubenpla/develop/petagramcoursera/util/preferences/PreferencesKeys.java
@@ -5,10 +5,7 @@ package com.rubenpla.develop.petagramcoursera.util.preferences;
  */
 
 public class PreferencesKeys {
-
     public static final String KEY_GET_CURRENT_USER = "KEY_GET_CURRENT_USER";
     public static final String KEY_PUT_CURRENT_USER = "KEY_PUT_CURRENT_USER";
-
-
-
+    public static final String KEY_POST_CURRENT_USER = "KEY_POST_CURRENT_USER";
  }
